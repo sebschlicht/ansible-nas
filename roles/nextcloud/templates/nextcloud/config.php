84d6181785a40d760372b7214d3c22ac14d523b9
Sebastian Schlicht <sebastian@jablab.de>
2020-10-20 00:10:44 +0200

    template config.php with memcaching (not fully working)

---
+++ roles/nextcloud/templates/nextcloud/config.php
@@ -0,0 +1,30 @@
+<?php
+$CONFIG = array (
+  'passwordsalt' => '{{ nc_passwordsalt }}',
+  'secret' => '{{ nc_secret }}',
+  'trusted_domains' =>
+  array (
+    0 => 'localhost',
+    1 => '{{ nas.domain }}',
+  ),
+  'datadirectory' => '{{ nextcloud.data_path }}',
+  'dbtype' => 'mysql',
+  'version' => '20.0.0.9',
+  'overwrite.cli.url' => 'http://{{ nas.domain }}',
+  'dbname' => '{{ db_name }}',
+  'dbhost' => 'localhost',
+  'dbport' => '',
+  'dbtableprefix' => 'oc_',
+  'mysql.utf8mb4' => true,
+  'dbuser' => '{{ db_user_name }}',
+  'dbpassword' => '{{ db_user_password }}',
+  'installed' => true,
+  'instanceid' => 'ocj3pxxeuxjq',
+  'memcache.local' => '\OC\Memcache\APCu',
+  'memcache.locking' => '\OC\Memcache\Redis',
+  // 'redis' => [
+  //     'host'     => '/var/run/redis/redis-server.sock',
+  //     'port'     => 0,
+  //     'timeout'  => 1.5,
+  // ],
+);
